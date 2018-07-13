c4deaef1aff0437bb7913e3f847d463d70ce419c
Angela Duran <duranangela19@gmail.com>
2018-07-13 10:21:02 -0600

    Add feature test for links to show page

--- spec/features/user_sees_all_books_spec.rb
+++ spec/features/user_sees_all_books_spec.rb
@@ -10,4 +10,13 @@ describe 'user visits the book index page' do
     expect(page).to have_content(book1.title)
     expect(page).to have_content(book2.title)
   end
+  it 'sees that book titles are links to show page' do
+    book1 = Book.create(title: "Ender's Game")
+
+    visit books_path
+
+    click_on book1.title
+
+    expect(current_path).to eq(book_path(book1))
+  end
 end
